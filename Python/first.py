e5e710b4b15b8e56f760c0c4f8f68ceb2de9ef37
ShrutiPethani123 <shrutipethani.royal@gmail.com>
2023-02-23 10:10:40 +0530

    py

---
+++ Python/first.py
@@ -0,0 +1,110 @@
+print("UJA")
+print('Uja')
+# print("uja') - invalid
+
+#  single line comment
+
+
+'''
+multi
+line 
+comment
+
+'''
+
+"""
+multi 
+line comment
+
+"""
+
+print("Good Morning")
+
+# This is Jeams's pen 
+
+print("This is Jeams's pen ")
+print('This is Jeams\'s pen ')
+
+'''
+Escape Sequence:
+
+\n
+\t
+\\
+
+'''
+
+print("My name is abc \n and I am a devloper")
+print("My name is abc \t and I am a devloper")
+print("My name is abc \\ and I am a devloper")
+print("My name is abc \\\ and I am a devloper")
+print('My name is abc " and I am a devloper')
+print("My name is abc \" and I am a devloper")
+
+
+# -----------------------------------
+
+print("java")
+print('python')
+
+print("java",end=" ")
+print('python')
+
+print("java",end=" and ")
+print('python')
+
+print('----------------------------------------------------')
+
+'''
+int 
+float
+str
+bool - False , True
+complex
+
+
+'''
+
+
+a=35246357357357
+print(a)
+print(type(a))
+
+
+a=52462462463587365782635783265872368572635827
+print(a)
+print(type(a))
+
+a=32123.2412
+print(a , type(a))
+
+a=351541351345456346.3513465134631463246
+print(a , type(a))
+
+
+b = "java"
+print(b , type(b))
+
+b = 'r'
+print(b , type(b))
+
+
+c = False
+print(c , type(c))
+
+
+
+d = 2+3.9j
+print(d)
+print(type(d))
+
+'''
+complex = real + imag
+
+real = 2
+imag = 3
+
+'''
+
+print(d.real)
+print(d.imag)
